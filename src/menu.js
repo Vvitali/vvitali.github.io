cbfb064885d75709f461a0815982216ccd3f769e
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-09-05 02:06:15 -0400

    Fixed to normal conditions
    Deleted unnecessary files and classes

--- src/menu.js
+++ src/menu.js
@@ -6,7 +6,7 @@ export class Menu extends React.Component{
 	<div className="Menu">	 
 	<div id="firsts">
 	<ul>
-	<li><a href = "#" onClick="{ }">Home</a></li>
+	<li><a href = "/">Home</a></li>
 	<li><a href = "#">Bio</a></li>
 	<li><a href = "#">Instagram</a></li>
 	<li><a href = "#">Contact</a></li>
