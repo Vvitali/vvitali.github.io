3538dfd430b8dbe6d6ca2238a2266ac641116983
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-09-09 23:59:49 -0400

    Normal view
    0.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Photo } from './photo';
+import './profile.css';
 import { Menu } from './menu';
+import {Photo} from "./photo";
 
 class App extends Component {
+	constructor(props){
+	super(props);
+	this.state = {oldtext: "Hello world!", body: "Nothing" };
+	this.changeBody = this.changeBody.bind(this);
+	}
+
+	changeBody(newBody){
+
+		var Instagram = <div className="PhotoArray"><Photo></Photo></div>
+  		var Biography = 
+  		<div className="BioStyle">
+  		<img src="/images/ava.jpg"/>
+  		<h1>Hello!</h1>
+  		<p >My name is Vitaliy Vorobyev, and I'm Full-stack web-developer. Now, you can see my home-page where you can find my contacts, 
+  		some of my projects, and just some interesting things</p>
+  		</div>
+
+		var Con = <div className="links">
+		<div className="links" id="github">
+		<img src="https://image.flaticon.com/icons/svg/25/25231.svg" style={{height: 100}}/>
+		<a href="https://github.com/Vvitali"><p>GitHub</p></a>
+		</div>
+
+		<div className="links" id="facebook">
+		<img src="http://www.freeiconspng.com/uploads/facebook-black-radius-transparent-26.png" style={{height: 100}}/>
+		<a href="https://www.facebook.com/profile.php?id=100001539428317"><p>Facebook</p></a>
+		</div>
+
+		<div className="links" id="linkin">
+		<img src="https://image.flaticon.com/icons/svg/34/34227.svg" style={{height: 100}}/>
+		<a href="https://www.linkedin.com/in/vitaliy-vorobyev-b5971210a"><p>LinkedIn</p></a>
+		</div>
+
+		<div id="resume">
+		<img src="http://izabelmar.com/wp-content/uploads/resume-logo.jpg" style={{ width:100}}/>
+		<a href="./Vitaliy Vorobyev SD resume.rtf"><p>Resume.rtf</p></a>
+		</div>
+		
+		</div>
+
+		switch(newBody){
+			case "Biography":
+			console.log("changeBody works "+ newBody)
+			this.setState({body: Biography });
+			break;
+			case "Instagram":
+			this.setState({body: Instagram });
+			break;
+			case "Con":
+			this.setState({body: Con });
+			break;
+			default: 
+			break;
+		}
+	}
+
   render() {
     return (	 
 	  <div className="App">
         <div className="Header"></div>
-		<div className="Menu"><Menu></Menu></div>
-		<div className="Body"></div>
-		<h1>Hello world!</h1>
-		<div className="PhotoArray"><Photo></Photo></div>
+		<div className="Menu"><Menu onClick={this.changeBody}></Menu></div>
+		<div  >
+		{this.state.body}
+		</div>		
 		</div>	 
     );
   }
--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 export class Menu extends React.Component{	
+	constructor(props){
+		super(props);
+		this.clickHandler = this.clickHandler.bind(this);
+	}
+
+	clickHandler(e){
+		var fine = e.target.getAttribute('id');
+		this.props.onClick(fine);
+	}
+
+
   render() {
+
+  	
 	return (
 	<div className="Menu">	 
 	<div id="firsts">
 	<ul>
-	<li><a href = "/">Home</a></li>
-	<li><a href = "#">Bio</a></li>
-	<li><a href = "#">Instagram</a></li>
-	<li><a href = "#">Contact</a></li>
+	<li><a href = "" onClick={this.clickHandler}>Home</a></li>
+	<li><a id="Biography" href = "#" onClick={this.clickHandler}>Bio</a></li>
+	<li><a id="Instagram" href = "#" onClick={this.clickHandler}>Instagram</a></li>
+	<li><a id="Con" href = "#" onClick={this.clickHandler}>Contact</a></li>
 	</ul>
 	</div>
 	</div>
