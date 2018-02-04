053cd0767f6888f027b3450f027551ce8d885248
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-04 00:59:22 -0500

    grid system implemented

--- src/App.js
+++ src/App.js
@@ -11,35 +11,49 @@ import Loadable from 'react-loadable';
 const Loading = () => <div>Loading...</div>;
 
 class App extends Component {
-	constructor(props){
-		super(props);
-	}
+constructor(props){
+super(props);
+}
 
-	Home = Loadable({
-		loader: () => import('./routes/Home'),
-		loading: Loading
-	});
-	Contact = Loadable({
-		loader: () => import('./routes/Contact'),
-		loading: Loading
-	});
+Home = Loadable({
+loader: () => import('./routes/Home'),
+loading: Loading
+});
+Contact = Loadable({
+loader: () => import('./routes/Contact'),
+loading: Loading
+});
+Portfolio = Loadable({
+loader: () => import('./routes/Portfolio'),
+loading: Loading
+});
 
-	render() {
-		return (
-			<div className="App">
-			<NavBar></NavBar>
-			<main>
-			<Router>
-			<Switch>
-			<Route exact path="/" component={this.Home}/>
-			<Route path="/contact" component={this.Contact}/>
-			</Switch>
-			</Router>
-			</main>
-			<Footer link="909a1c" vlink="#cecece"></Footer>
-			</div>	 
-			);
-	}
+render() {
+return (
+<div className="App">
+	<NavBar></NavBar>
+	<main>
+		<div className="container">
+			<div className="row">
+				<div className="col s3"></div>
+				<div className="col s6">
+					<Router>
+						<Switch>
+							<Route exact path="/" component={this.Home}/>
+							<Route path="/contact" component={this.Contact}/>
+							<Route path="/portfolio" component={this.Portfolio}/>
+						</Switch>
+					</Router>
+				</div>
+				<div className="col s3"></div>
+			</div>
+		</div>
+
+	</main>
+	<Footer link="909a1c" vlink="#cecece"></Footer>
+</div>	 
+);
+}
 }
 
 export default App;
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -12,7 +12,7 @@ export class NavBar extends Component{
 			<a href="#" className="brand-logo center">Vitaliy Vorobyev</a>
 			<ul id="nav-mobile" className="left hide-on-med-and-down">
 			<li><a href="/">Home</a></li>
-			<li><a href="badges.html">Components</a></li>
+			<li><a href="/portfolio">Portfolio</a></li>
 			<li><a href="/contact">Contact me</a></li>
 			</ul>
 			</div>
