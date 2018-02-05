6a88bd3c2734ce1b34017231a83d59a64ac236d9
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-05 15:22:12 -0500

    reduced size of ava.jpg, stateless footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Photo} from "./photo";
-import {Footer} from "./Components/Footer/Footer";
+import Footer from "./Components/Footer/Footer";
 import {NavBar} from "./Components/NavBar/NavBar";
 
 //routing
@@ -11,50 +11,50 @@ import Loadable from 'react-loadable';
 const Loading = () => <div>Loading...</div>;
 
 class App extends Component {
-constructor(props){
-super(props);
-}
+	constructor(props){
+		super(props);
+	}
 
-Home = Loadable({
-loader: () => import('./routes/Home'),
-loading: Loading
-});
-Contact = Loadable({
-loader: () => import('./routes/Contact'),
-loading: Loading
-});
-Portfolio = Loadable({
-loader: () => import('./routes/Portfolio'),
-loading: Loading
-});
+	Home = Loadable({
+		loader: () => import('./routes/Home'),
+		loading: Loading
+	});
+	Contact = Loadable({
+		loader: () => import('./routes/Contact'),
+		loading: Loading
+	});
+	Portfolio = Loadable({
+		loader: () => import('./routes/Portfolio'),
+		loading: Loading
+	});
 
-render() {
-return (
-<div className="App">
-	<NavBar></NavBar>
-	<main>
-		<div className="container">
+	render() {
+		return (
+			<div className="App">
+			<NavBar></NavBar>
+			<main>
+			<div className="container">
 			<div className="row"></div>
 			<div className="row">
-				<div className="col s3"></div>
-				<div className="col s6">
-					<Router>
-						<Switch>
-							<Route exact path="/" component={this.Home}/>
-							<Route path="/contact" component={this.Contact}/>
-							<Route path="/portfolio" component={this.Portfolio}/>
-						</Switch>
-					</Router>
-				</div>
-				<div className="col s3"></div>
+			<div className="col s3"></div>
+			<div className="col s6">
+			<Router>
+			<Switch>
+			<Route exact path="/" component={this.Home}/>
+			<Route path="/contact" component={this.Contact}/>
+			<Route path="/portfolio" component={this.Portfolio}/>
+			</Switch>
+			</Router>
 			</div>
-		</div>
+			<div className="col s3"></div>
+			</div>
+			</div>
 
-	</main>
-	<Footer link="909a1c" vlink="#cecece"></Footer>
-</div>	 
-);
-}
+			</main>
+			<Footer link="909a1c" vlink="#cecece"></Footer>
+			</div>	 
+			);
+	}
 }
 
 export default App;
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-export class Footer extends React.Component{
-render() {  	
-return (
-<footer className="page-footer">
-	<div className="footer-copyright">
+const Footer = ()=>{
+	return (
+		<footer className="page-footer">
+		<div className="footer-copyright">
 		<div className="container">
-			<p>Copyright by <a  href="https://www.linkedin.com/in/vitaliy-vorobyev" target="_blank">Vvitali</a> @ 2017</p>
+		<p>Copyright by <a  href="https://www.linkedin.com/in/vitaliy-vorobyev" target="_blank">Vvitali</a> @ 2017</p>
 		</div>
-	</div>
-</footer>
-)	
-};  
+		</div>
+		</footer>
+		)	
 }
+export default Footer;
