d6e8321500eb4a6af10dc44bb3fe5f0a531c4222
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-05 23:24:12 -0500

    style of portfolio component, links to project

--- src/Components/PortfolioItem/index.js
+++ src/Components/PortfolioItem/index.js
@@ -2,22 +2,23 @@ import React, {Component} from 'react';
 import './style.css';
 
 class Project extends Component{
-render(){
-return (
-<div className="col s12 m4 ProjectItem">
-	<div className="card">
-		<div className="card-image">
+	render(){
+		return (
+			<div className="col s12 m4 ProjectItem">
+			<a href={this.props.url} target="_blank">
+			<div className="card">
+			<div className="card-image">
 			<img src={this.props.imgSrc? this.props.imgSrc : "http://via.placeholder.com/650x350"}/>
-			<span className="card-title">{this.props.title? this.props.title : "Future project"}</span>
-		</div>
-		<div className="card-content">
-			<p>{this.props.descr? this.props.descr :"Very cool future project! " + this.props.title}
-			</p>
-		</div>
-	</div>
-</div> 
-);
-};
+			<span className="card-title grey-text  text-darken-1">{this.props.title? this.props.title : "Future project"}</span>
+			</div>
+			<div className="card-content">
+			<p>{this.props.descr? this.props.descr :"Very cool future project! " + this.props.title}</p>
+			</div>
+			</div>
+			</a>
+			</div> 
+			);
+	};
 };
 
 export default Project;
--- src/routes/Portfolio/index.js
+++ src/routes/Portfolio/index.js
@@ -8,9 +8,9 @@ class Portfolio extends Component{
 	render(){
 		return(
 			<div className="project-list row">
-			<Project title="TheCryptoShop" imgSrc="https://github.com/Vvitali/TheCryptoShop/raw/master/media/main.gif" descr="Online shop where you can buy eBay goods using Bitcoin!"></Project>
-			<Project title="Secret-Santa" imgSrc="https://raw.githubusercontent.com/dfarrenk/ProjectOne/master/screenshot/index.png" descr="Application for exchanging Christmas presents anonymously"></Project>
-			<Project title="mealCrave" imgSrc="https://raw.githubusercontent.com/schapm16/mealCrave/development/media/main.jpg" descr="Find dishes from local restaurants that have been curated and posted by users"></Project>
+			<Project url="https://github.com/Vvitali/TheCryptoShop" title="TheCryptoShop" imgSrc="https://github.com/Vvitali/TheCryptoShop/raw/master/media/main.gif" descr="Online shop where you can buy eBay goods using Bitcoin!"></Project>
+			<Project url="https://github.com/Vvitali/Secret-Santa" title="Secret-Santa" imgSrc="https://raw.githubusercontent.com/dfarrenk/ProjectOne/master/screenshot/index.png" descr="Application for exchanging Christmas presents anonymously"></Project>
+			<Project url="https://github.com/Vvitali/mealCrave" title="mealCrave" imgSrc="https://raw.githubusercontent.com/schapm16/mealCrave/development/media/main.jpg" descr="Find dishes from local restaurants that have been curated and posted by users"></Project>
 			<Project></Project>
 			<Project></Project>
 			</div>		
