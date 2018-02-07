544ef40fbd172f2dc8e1f7f9dcca577e3fcc2c44
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-07 17:26:32 -0500

    body parser added

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import React from "react";
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+
 const router = express.Router();
 
-router.get("/portfolio", (req, res)=>{
+router.get(`/`,(req, res)=>{
+	console.log("Request to //");
+	res.send("home!")
+});
+router.get(`/portfolio`, (req, res)=>{
 	res.send("portfolio");
+});
+router.get(`/contacts`, (req, res)=>{
+	res.send("contacts");
+});
+router.get(`/home`, (req, res)=>{
+	res.send("home");
 });
 
 export default router;
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const DEBUG = true;
 import express from "express";
-import React from "react";
-import ReactDOM from 'react-dom';
 import htmlRoutes from "./controllers/htmlRoutes";
 console.log("Server-file load: ok!");
 const app = express();
 const PORT = 8080;
 app.all("*", htmlRoutes);
-app.get(`/`,(req, res)=>{
-	console.log("Request to //");
-	res.send("YO!")
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.listen(PORT, ()=>{
 	console.log(`Server started at port: ${PORT}`);
 })
