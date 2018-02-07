95839c8d6c826dca0f225d1e19518d5d39f5d3da
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-07 16:42:59 -0500

    basic server

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,20 @@
 "use strict";
 
-var a = 0;
-console.log(a);
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var DEBUG = true;
+
+console.log("Server-file load: ok!");
+var app = (0, _express2.default)();
+var PORT = 8080;
+app.get("/", function (req, res) {
+	console.log("Request to //");
+	res.send("YO!");
+});
+app.listen(PORT, function () {
+	console.log("Server started at port: " + PORT);
+});
--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-let a = 0;
-console.log(a);
+const DEBUG = true;
+import express from "express";
+console.log("Server-file load: ok!");
+const app = express();
+const PORT = 8080;
+app.get(`/`,(req, res)=>{
+	console.log("Request to //");
+	res.send("YO!")
+});
+app.listen(PORT, ()=>{
+	console.log(`Server started at port: ${PORT}`);
+})
+
