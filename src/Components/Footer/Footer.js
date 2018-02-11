f315575fb3c6d6a15cd564480133fcf9bbde5fe7
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-11 04:21:46 -0500

    storyline: started

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = ()=>{
 	return (
-		<nav className="navbar navbar-inverse navbar-fixed-bottom footer">
+		<nav className="navbar navbar-inverse navbar-fixed-bottom footer page-footer" style={{ marging: "15px"}}>
 		<div className="container-fluid">
 		<div className="navbar-header">
 		<a className="navbar-brand " href="https://www.linkedin.com/in/vitaliy-vorobyev/">Vitaliy Vorobyev</a>
--- src/routes/AboutMe/index.js
+++ src/routes/AboutMe/index.js
@@ -1,15 +1,25 @@
 import React from "react"
 import MapComponent from "../../Components/Map";
+import "./style.css";
 
 const AboutMe = ()=>{
 	const isMobile = window.innerWidth <= 500;
-	console.log(window.innerWidth)
 	return(
-		<div className="row">
-		<h1 className="page-header bottom">About me</h1>
-		<p>My name is Vitaliy Vorobyev, and I'm a Full-stack web-developer.</p>
-		<h3>My journey! <span style={{fontSize: "14px"}}>(hint: {isMobile? "hold your finger couple seconds over any marker":"hover the cursor over any marker"})</span></h3>
+		<div className="row text-left" >
+		<div style={ isMobile? { paddingLeft: "15px"} : {padding: "0px"}}>
+		<h1 className="bottom">About me</h1>
+		<hr/>
+		<p>My name is Vitaliy Vorobyev (I think, you already knew it <img className="emoji"src="http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/grinning-cat-face-with-smiling-eyes.png" alt=""/> ), and I'm very interested in computers, and computer science.
+		<br/>
+		I was born in  <a href="https://en.wikipedia.org/wiki/Kazakhstan" alt="Republic of Kazakhstan" target="_blank">Republic of Kazakhstan</a>, lived for a while in Russian Federation, and now I live in The United States.
+		<br/>
+		When i was 8 years old, my mother has bought me my first computer, and I've started to learn digital world!</p>
+		<h3 className="">My journey! <span style={{fontSize: "14px"}}>(hint: {isMobile? "hold your finger couple seconds over any marker":"hover the cursor over any marker"})</span></h3>
+		</div>
 		<MapComponent/>
+
+		<span><em>1999: In my childhood I had a lot of toys, and electric devices (like cassette player), one day I've found a screwdriver <img className="emoji" src="https://cdn.emojidex.com/emoji/seal/%28flat_head%29screw_driver.png" alt=""/>, and after couple hours my parents found me on the floor surrounded by numbers of details</em></span>
+
 		</div>
 		);
 }
