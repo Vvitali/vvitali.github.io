55c5e81654cdb07711fce93d145af4e57badb9b5
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-11 02:43:04 -0500

    pictures added to the array

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -6,22 +6,26 @@ const places = [
 {
 	name: "Ust-Kamenogorsk, KZ",
 	position: { lat: 49.976515, lng: 82.571675 },
-	title: "I was born here!"
+	title: "I was born here!",
+	pic: "https://www.justgorussia.co.uk/userfiles/images/russia/russian_regions/altay.jpg"
 }
 ,{
 	name: "Bryansk, RU",
 	position: { lat: 53.304458, lng: 34.312140 },
-	title: "I lived here for 5 years, and studied computer science"
+	title: "I lived here for 5 years, and studied computer science",
+	pic: "https://imgp.golos.io/0x0/http://i.imgsafe.org/fc06379e78.jpg"
 }
 ,{
 	name: "Portland, OR",
 	position: { lat: 45.570655, lng: -122.799300 },
-	title: "I lived here for 2 years"
+	title: "I lived here for 2 years",
+	pic: "http://greatruns.com/wp-content/uploads/2016/11/Mt-Hood-and-Portland-waterfront-Agape-Voyage-700x447.jpeg"
 }
 ,{
 	name: "Charlotte, NC",
 	position: { lat: 35.221194, lng: -80.840227 },
-	title: "Now, I live here, and I'm studying computer science again (Web-development)"
+	title: "Now, I live here, and I'm studying computer science again (Web-development)",
+	pic:" http://www.charlottesgotalot.com/sites/default/master/files/styles/sliderkit_main_slide/public/Skyline_Night_970x550.jpg?itok=jmydaRdZ"
 }
 ];
 
@@ -42,7 +46,7 @@ class MapComponent extends React.Component{
 	render(){
 		let renderedPlaces = [];
 		for(let place of places){
-			renderedPlaces.push(<MapObject key={place.name} name={place.name} lat={ place.position.lat } lng={place.position.lng } text={ place.title }/>); 
+			renderedPlaces.push(<MapObject key={place.name} name={place.name} lat={ place.position.lat } lng={place.position.lng } pic={ place.pic} text={ place.title }/>); 
 		};
 		console.log("start");
 		return (
--- src/Components/Map/object.js
+++ src/Components/Map/object.js
@@ -5,35 +5,36 @@ const showText = ()=>{
 
 class mObject extends React.Component{
 
-constructor(props){
-super(props);
-console.log(this.props.text)
-this.state = {displayed: false};
-}
-onMouseEnterHandler = ()=>{
-this.setState({
-displayed: true
-});
-}
+	constructor(props){
+		super(props);
+		console.log(this.props.text)
+		this.state = {displayed: false};
+	}
+	onMouseEnterHandler = ()=>{
+		this.setState({
+			displayed: true
+		});
+	}
 
-onMouseLeaveHandler = ()=>{
-this.setState({
-displayed: false
-});
-}
-render(){
-return (
-<div>
-	<img onMouseEnter={this.onMouseEnterHandler} onMouseLeave={this.onMouseLeaveHandler} src="/images/map.png" alt="arrow" style={{position: 'relative', width: "20px", top: -20, left: -10}}/>
-	<div className={this.state.displayed? "card" : "card hidden"} style={{width: "18rem", backgroundColor: "white", borderRadius: "5px"}}>
-		<div className="card-body">
+	onMouseLeaveHandler = ()=>{
+		this.setState({
+			displayed: false
+		});
+	}
+	render(){
+		return (
+			<div>
+			<img onMouseEnter={this.onMouseEnterHandler} onMouseLeave={this.onMouseLeaveHandler} src="/images/map.png" alt="arrow" style={{position: 'relative', width: "20px", top: -20, left: -10}}/>
+			<div className={this.state.displayed? "card" : "card hidden"} style={{width: "18rem", backgroundColor: "white", borderRadius: "5px"}}>
+			<div className="card-body">
+			<img style={{ width:"100%" }} src={this.props.pic}/>
 			<h5 className="card-title">{this.props.name}</h5>
-			<p className="card-text">{this.props.text}</p>
-		</div>
-	</div>
-</div>
-);
-}
+			<p className="card-text" style={{paddingBottom: "15px"}}>{this.props.text}</p>
+			</div>
+			</div>
+			</div>
+			);
+	}
 
 };
 
