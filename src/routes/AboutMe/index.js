6b6614b11a0019fda2a277b88d2dbb8023cde59c
Vitaliy Vorobyev <vvitali26@gmail.com>
2018-02-12 01:20:22 -0500

    updated story

--- src/routes/AboutMe/index.js
+++ src/routes/AboutMe/index.js
@@ -10,10 +10,15 @@ const AboutMe = ()=>{
 		<h1 className="bottom">About me</h1>
 		<hr/>
 		<p>My name is Vitaliy Vorobyev (I think, you already knew it <img className="emoji"src="http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/grinning-cat-face-with-smiling-eyes.png" alt=""/> ), and I'm very interested in computers, and computer science.
-		<br/>
 		I was born in  <a href="https://en.wikipedia.org/wiki/Kazakhstan" alt="Republic of Kazakhstan" target="_blank">Republic of Kazakhstan</a>, lived for a while in Russian Federation, and now I live in The United States.
 		<br/>
-		When i was 8 years old, my mother has bought me my first computer, and I've started to learn digital world!</p>
+		<h3>Origin</h3>
+		When i was 8 years old, my mother has bought me my first computer, and I've started to learn digital world, I was attending computer science class at my first school, and there I learned foundations of CS (like information, types of information, variables, numeral systems, algorithms, and etc), wrote my first computer programm (I used program language called <a href="https://en.wikipedia.org/wiki/BASIC" target="_blank">BASIC</a>). </p>
+		<h3>Middle school</h3>
+		<p>Unfortunatly, when I went to middle school, my family moved to another place, and at new school I had very low level CS-class, so I had to educate myself, it was hard and inconvenient, because this period of time in my home town (<a href="https://en.wikipedia.org/wiki/Oskemen" target="_blank">Ust'-Kamenogorks</a>)
+		
+		</p>
+
 		<h3 className="">My journey! <span style={{fontSize: "14px"}}>(hint: {isMobile? "hold your finger couple seconds over any marker":"hover the cursor over any marker"})</span></h3>
 		</div>
 		<MapComponent/>
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 class Home extends Component{
 	render(){
 		return(
-			<div className="BioStyle">
-			<h1>Hello!</h1>
-			<img src="/images/ava.jpg" alt="Vitaliy's face"/>
-			<p>My name is Vitaliy Vorobyev, and I'm a Full-stack web-developer. Now, you can see my home-page where you can find my contacts, 
+			<div className="BioStyle row text-left">
+			<h1 className="bottom">Hello!</h1>
+			<hr/>	
+			<img src="/images/ava.jpg" alt="Vitaliy's face" style={{float: "left", paddingRight: "15px"}}/>
+			<p>My name is Vitaliy Vorobyev, and I'm a Full-stack web-developer. <br/>Now, you can see my home-page where you can find my contacts, 
 			some of my projects, and just some interesting things</p>
 			</div> 
 			)
