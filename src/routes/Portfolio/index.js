d81bd9fb63ff4d84c3a775e20319bc1425881945
Vvitali <vvitali26@gmail.com>
2018-03-01 18:47:44 -0500

    new photo, links, description, added one of old designs to portfolio

--- src/routes/Portfolio/index.js
+++ src/routes/Portfolio/index.js
@@ -5,11 +5,11 @@ class Portfolio extends Component{
 	render(){
 		return(
 			<div className="project-list row">
-			<Project url="https://github.com/Vvitali/TheCryptoShop" title="TheCryptoShop" imgSrc="https://github.com/Vvitali/TheCryptoShop/raw/master/media/main.gif" descr="Online shop where you can buy eBay goods using Bitcoin!"></Project>
-			<Project url="https://github.com/Vvitali/Secret-Santa" title="Secret-Santa" imgSrc="https://raw.githubusercontent.com/dfarrenk/ProjectOne/master/screenshot/index.png" descr="Application for exchanging Christmas presents anonymously"></Project>
-			<Project url="https://github.com/Vvitali/mealCrave" title="mealCrave" imgSrc="https://raw.githubusercontent.com/schapm16/mealCrave/development/media/main.jpg" descr="Find dishes from local restaurants that have been curated and posted by users"></Project>
-			<Project title="Future project 1" descr="Great future app #1"></Project>
-			<Project title="Future project 2" descr="Great future app #2"></Project>
+			<Project url="https://thecryptoshop.herokuapp.com" title="TheCryptoShop" imgSrc="https://github.com/Vvitali/TheCryptoShop/raw/master/media/main.gif" descr="Online shop where you can buy eBay goods using Bitcoin! Back-end\Full-stack (Currently: Beta)"></Project>
+			<Project url="https://vvitali.github.io/Secret-Santa/" title="Secret-Santa" imgSrc="/images/schreenshots/thesecret-santa.jpg" descr="Application for exchanging Christmas presents anonymously. Back-end\Full-stack "></Project>
+			<Project url="https://meal-crave.herokuapp.com" title="mealCrave" imgSrc="/images/schreenshots/themealCrave.jpg" descr="Find dishes from local restaurants that have been curated and posted by users. Back-end"></Project>
+			<Project title="The Matrix Hangman" descr="Matrix style!" imgSrc="/images/schreenshots/thematrix-hangman.jpg" url="https://vvitali.github.io/Hangman/"></Project>
+			<Project title="Future project 2" descr="Great future app #1"></Project>
 			</div>		
 			);
 	};
