c37f3fb959b1975fbacf21bf778c072b3ec4e11e
Vvitali <vvitali26@gmail.com>
2018-03-01 20:30:29 -0500

    appears effect

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -8,9 +8,9 @@ class Home extends Component{
 			<div className="BioStyle row text-left">
 			<h1 className="bottom" style={style}>Hello!</h1>
 			<hr/>
-			<div className="col-sm-12 col-md-6" style={style}><p>My name is Vitaliy Vorobyev, and I'm a Full-stack web-developer. <br/>Now, you can see my home-page where you can find my contacts, 
+			<div className="col-sm-12 col-md-6" style={style}><p className="animated">My name is Vitaliy Vorobyev, and I'm a Full-stack web-developer. <br/>Now, you can see my home-page where you can find my contacts, 
 			some of my projects, and just some interesting things</p></div>
-			<div className="col-sm-12 col-md-6 text-center"><img className="shadow" src="/images/ava.jpg" alt="Vitaliy's face"/></div>
+			<div className="col-sm-12 col-md-6 text-center animated"><img className="shadow" src="/images/ava.jpg" alt="Vitaliy's face"/></div>
 			</div> 
 			)
 	};
