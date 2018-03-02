2c864e82cc59ae1f9fa2edfa730585eb18772824
Vvitali <vvitali26@gmail.com>
2018-03-01 23:49:03 -0500

    text changes, no footer, static image for cryptoshop

--- src/routes/Portfolio/index.js
+++ src/routes/Portfolio/index.js
@@ -5,7 +5,7 @@ class Portfolio extends Component{
 	render(){
 		return(
 			<div className="project-list row">
-			<Project url="https://thecryptoshop.herokuapp.com" title="TheCryptoShop" imgSrc="https://github.com/Vvitali/TheCryptoShop/raw/master/media/main.gif" descr="Online shop where you can buy eBay goods using Bitcoin! Back-end\Full-stack (Currently: Beta)"></Project>
+			<Project url="https://thecryptoshop.herokuapp.com" title="TheCryptoShop" imgSrc="./images/schreenshots/thecryptoshop.jpg" descr="Online shop where you can buy eBay goods using Bitcoin! Back-end\Full-stack (Currently: Beta)"></Project>
 			<Project url="https://vvitali.github.io/Secret-Santa/" title="Secret-Santa" imgSrc="./images/schreenshots/thesecret-santa.jpg" descr="Application for exchanging Christmas presents anonymously. Back-end\Full-stack "></Project>
 			<Project url="https://meal-crave.herokuapp.com" title="mealCrave" imgSrc="./images/schreenshots/themealCrave.jpg" descr="Find dishes from local restaurants that have been curated and posted by users. Back-end"></Project>
 			<Project title="The Matrix Hangman" descr="Matrix style!" imgSrc="./images/schreenshots/thematrix-hangman.jpg" url="https://vvitali.github.io/Hangman/"></Project>
