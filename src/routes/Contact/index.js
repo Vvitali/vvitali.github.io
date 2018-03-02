d55f8da7ee3207c78bc38051bc00fd8b74cba3ae
Vvitali <vvitali26@gmail.com>
2018-03-01 21:14:07 -0500

    fixed image links for GP

--- src/routes/Contact/index.js
+++ src/routes/Contact/index.js
@@ -6,30 +6,30 @@ const Contact =()=>{
 		<div className="links">
 		<div className="links" id="github">
 		<a href="https://github.com/Vvitali" target="_blank" rel="noopener noreferrer">
-		<img src="/images/github_logo.svg" style={{height: 100}} alt="github"/>
+		<img src="./images/github_logo.svg" style={{height: 100}} alt="github"/>
 		<p>GitHub</p>
 		</a>
 		</div>
 
 		<div className="links" id="facebook">
 		<a href="https://www.facebook.com/profile.php?id=100001539428317" target="_blank" rel="noopener noreferrer">
-		<img src="/images/facebook_logo.png" style={{height: 100}} alt="facebook"/>
+		<img src="./images/facebook_logo.png" style={{height: 100}} alt="facebook"/>
 		<p>Facebook</p>
 		</a>
 		</div>
 
 		<div className="links" id="linkin">
 		<a href="https://www.linkedin.com/in/vitaliy-vorobyev-b5971210a" target="_blank" rel="noopener noreferrer">
-		<img src="/images/linkedin_logo.svg" style={{height: 100}} alt="linkedin"/>
+		<img src="./images/linkedin_logo.svg" style={{height: 100}} alt="linkedin"/>
 		<p>LinkedIn</p>
 		</a>
 		</div>
 
 		<div id="resume">
-		<img src="/images/resume-logo.jpg" style={{ width:100}} alt="resume"/>
+		<img src="./images/resume-logo.jpg" style={{ width:100}} alt="resume"/>
 		</div>
 		<a href="https://docs.google.com/document/d/1SHh8_B4WcXeDzxFUKNbYgjo2rnQRN6oo4S6AtJd_vdk/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
-		<img src="/images/gdrive.png" style={{ width:100}} alt="facebook"/>
+		<img src="./images/gdrive.png" style={{ width:100}} alt="facebook"/>
 		<p>Watch it on Google Docs resume</p>
 		</a>
 		<a href="https://docs.google.com/document/d/1SHh8_B4WcXeDzxFUKNbYgjo2rnQRN6oo4S6AtJd_vdk/export?format=pdf" target="_blank" rel="noopener noreferrer">	
