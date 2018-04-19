37689686500d5f58b41468c389a116905237037e
Vvitali <vvitali26@gmail.com>
2018-04-19 15:47:06 -0400

    new project added: nw sterling homes

--- src/routes/Portfolio/index.js
+++ src/routes/Portfolio/index.js
@@ -5,7 +5,7 @@ class Portfolio extends Component{
 	render(){
 		return(
 			<div className="project-list row">
-			<Project url="http://www.nwsterlinghomes.com" title="NW Sterling Homes" imgSrc="./images/schreenshots/nwsterlinghomes.jpg" descr="North West Sterling Homes is a whole home building and remodeling company with more than 25 years of experience based in Portland, Oregon."></Project>
+			<Project url="http://www.nwsterlinghomes.com" title="NW Sterling Homes" imgSrc="./images/schreenshots/nwsterlinghomes.jpg" descr="North West Sterling Homes is a whole home building and remodeling company with more than 25 years of experience based in Portland, Oregon. Web-site has been created from scratch"></Project>
 			<Project url="https://thecryptoshop.herokuapp.com" title="TheCryptoShop" imgSrc="./images/schreenshots/thecryptoshop.jpg" descr="Online shop where you can buy eBay goods using Bitcoin! Back-end\Full-stack (Currently: Beta)"></Project>
 			<Project url="https://vvitali.github.io/Secret-Santa/" title="Secret-Santa" imgSrc="./images/schreenshots/thesecret-santa.jpg" descr="Application for exchanging Christmas presents anonymously. Back-end\Full-stack "></Project>
 			<Project url="https://meal-crave.herokuapp.com" title="mealCrave" imgSrc="./images/schreenshots/themealCrave.jpg" descr="Find dishes from local restaurants that have been curated and posted by users. Back-end"></Project>
