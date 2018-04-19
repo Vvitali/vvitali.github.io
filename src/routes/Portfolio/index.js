95eb22741c41f713bfc702f0447b7ccd78132fe8
Vvitali <vvitali26@gmail.com>
2018-04-19 17:22:49 -0400

    new project added

--- src/routes/Portfolio/index.js
+++ src/routes/Portfolio/index.js
@@ -6,10 +6,11 @@ class Portfolio extends Component{
 		return(
 			<div className="project-list row">
 			<Project url="http://www.nwsterlinghomes.com" title="NW Sterling Homes" imgSrc="./images/schreenshots/nwsterlinghomes.jpg" descr="North West Sterling Homes is a whole home building and remodeling company with more than 25 years of experience based in Portland, Oregon. Web-site has been created from scratch"></Project>
-			<Project url="https://thecryptoshop.herokuapp.com" title="TheCryptoShop" imgSrc="./images/schreenshots/thecryptoshop.jpg" descr="Online shop where you can buy eBay goods using Bitcoin! Back-end\Full-stack (Currently: Beta)"></Project>
+			<Project url="https://thecryptoshop.herokuapp.com" title="TheCryptoShop" imgSrc="./images/schreenshots/thecryptoshop.jpg" descr="E-commerce blockchain project allows user to buy eBay goods using Bitcoin  Back-end\Full-stack (Currently: Beta)"></Project>
 			<Project url="https://vvitali.github.io/Secret-Santa/" title="Secret-Santa" imgSrc="./images/schreenshots/thesecret-santa.jpg" descr="Application for exchanging Christmas presents anonymously. Back-end\Full-stack "></Project>
 			<Project url="https://meal-crave.herokuapp.com" title="mealCrave" imgSrc="./images/schreenshots/themealCrave.jpg" descr="Find dishes from local restaurants that have been curated and posted by users. Back-end"></Project>
 			<Project title="The Matrix Hangman" descr="Matrix style!" imgSrc="./images/schreenshots/thematrix-hangman.jpg" url="https://vvitali.github.io/Hangman/"></Project>
+			<Project imgSrc="./images/schreenshots/sns.jpg" title="Stalker network system PDA (orignial name: 'Сталкерская сеть') " descr="Web-site support, and social network windows application for game fan club. Unfortunatly, after several versions, project closed. Years: 2009-2013"></Project>
 			<Project title="Future project 2" descr="Great future app #1"></Project>
 
 			</div>		
