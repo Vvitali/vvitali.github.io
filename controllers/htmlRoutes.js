38705bb1c4066d3e0d2cb9a03ea6be402f56b7b4
Vvitali <vvitali26@gmail.com>
2018-05-10 22:29:28 -0400

    changed the flow of the project

--- controllers/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get(`/blog`, (req, res)=>{
-	res.send("home");
-});
-
-export default router;
--- dist/README.md
+++
@@ -1,4 +0,0 @@
-Used: React.js, Node.js
-
-Link to heroku instance: https://serene-wave-89886.herokuapp.com
-Link to GP: https://vvitali.github.io/Personal-web-site/
